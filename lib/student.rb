e13be485b49a16b3f5a5a8b64eda21dbd5080c19
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-19 04:54:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,12 @@ class Student  < User
   attr_accessor :knowledge
 
   def initialize(knowledge)
-    knowledge = []
+    @knowledge = []
   end
+
+  def learn(string)
+     @knowledge << string
+   end
 
 
 end
